2070f8e3d1504359d841de68c06e3adad72ef48a
keeb-nekoyamaou <131568590+keeb-nekoyamaou@users.noreply.github.com>
2024-07-28 14:58:02 +0900

    Add files via upload

---
+++ neo/ergo/keymaps/vial/config.h
@@ -0,0 +1,40 @@
+/* 
+Copyright 2024 NEO
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x66, 0x43, 0x8B, 0x3D, 0xF4, 0xFE, 0x18, 0xBB}
+#define VIAL_UNLOCK_COMBO_ROWS {0, 0}
+#define VIAL_UNLOCK_COMBO_COLS {0, 1}
+
+
+/* WS2812 Underglow Matrix options */
+#ifdef RGBLIGHT_ENABLE
+#define RGB_DISABLE_WHEN_USB_SUSPENDED
+#endif
+
+
+/* WS2812 Underglow Matrix options */
+#ifdef RGB_MATRIX_ENABLE
+#define RGBLIGHT_LAYERS        // レイヤーとの連動機能の有効化
+#define RGBLIGHT_MAX_LAYERS 16  // 連動するレイヤー数(最大32)
+
+#define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+#define RGB_MATRIX_KEYPRESSES
+
+#define RGB_DISABLE_WHEN_USB_SUSPENDED
+#endif
---
+++ neo/ergo/readme.md
@@ -0,0 +1,26 @@
+# neo ergo wired - PCB
+
+![neo ergo wired]()
+
+An Alice style board with arrowkeys made by NEO Studio.
+
+* Keyboard Maintainer: [NEO Studio](https://github.com/owlab-git)
+* Hardware Supported: neo ergo wired PCB
+
+Source code for Vial port
+
+Please note that using source code is requiring to take full responsibility for your own action. Unsuccessful flash results in a bricked PCB and your fantastic keyboard life with Neo Ergo ends up at this moment.
+
+VERY IMPORTANANT
+My environment is as followed;
+- Wired Non-flex cut Hotswap PCB
+- Windows
+- QMK Tool Box to flash firmware
+
+I am not responsible for any loss, any damage, any disadvantage or bricked PCB caused by using the information and source code on my page. Please consider the risks and "TAKE FULL RESPONSIBILITY FOR YOUR OWN ACTION".
+
+Make example for this keyboard (after setting up your build environment):
+
+    make neo/ergo:vial
+
+For more information, please see https://note.com/nekoyamaou/n/n73237d7be569.
