68c1c3132f7ce9f3a77e05585d57cfa0872344d4
nekoyamao <131568590+nekoyamao@users.noreply.github.com>
2024-09-08 15:27:37 +0900

    Update matrix.c

--- planck/rev7/matrix.c
+++ planck/rev7/matrix.c
@@ -15,12 +15,10 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "gpio.h"
-#include "hal_pal.h"
-#include "hal_pal_lld.h"
-#include "quantum.h"
+#include "matrix.h"
+#include <hal_pal.h>
 #include <math.h>
-#include "encoder.h"
+#include "wait.h"
 
 // STM32-specific watchdog config calculations
 // timeout = 31.25us * PR * (RL + 1)
@@ -37,39 +35,27 @@
 #   define PLANCK_WATCHDOG_TIMEOUT 1.0
 #endif
 
-#if !defined(PLANCK_ENCODER_SETTLE_PIN_DELAY)
-#   define PLANCK_ENCODER_SETTLE_PIN_DELAY 20
-#endif
-
 /* matrix state(1:on, 0:off) */
 static pin_t matrix_row_pins[MATRIX_ROWS] = MATRIX_ROW_PINS;
 static pin_t matrix_col_pins[MATRIX_COLS] = MATRIX_COL_PINS;
 
 static matrix_row_t matrix_inverted[MATRIX_COLS];
 
-
-#ifdef ENCODER_ENABLE
-int8_t  encoder_LUT[]     = {0, -1, 1, 0, 1, 0, 0, -1, -1, 0, 0, 1, 0, 1, -1, 0};
-uint8_t encoder_state[8]  = {0};
-int8_t  encoder_pulses[8] = {0};
-uint8_t encoder_value[8]  = {0};
-#endif
-
 void matrix_init_custom(void) {
     // actual matrix setup - cols
     for (int i = 0; i < MATRIX_COLS; i++) {
-        setPinOutput(matrix_col_pins[i]);
-        writePinLow(matrix_col_pins[i]);
+        gpio_set_pin_output(matrix_col_pins[i]);
+        gpio_write_pin_low(matrix_col_pins[i]);
     }
 
     // rows
     for (int i = 0; i < MATRIX_ROWS; i++) {
-        setPinInputLow(matrix_row_pins[i]);
+        gpio_set_pin_input_low(matrix_row_pins[i]);
     }
 
     // encoder A & B setup
-    setPinInputLow(B12);
-    setPinInputLow(B13);
+    gpio_set_pin_input_low(B12);
+    gpio_set_pin_input_low(B13);
 
 #ifndef PLANCK_WATCHDOG_DISABLE
     wdgInit();
@@ -81,7 +67,6 @@ void matrix_init_custom(void) {
     wdgStart(&WDGD1, &wdgcfg);
 #endif
 }
-
 
 bool matrix_scan_custom(matrix_row_t current_matrix[]) {
 #ifndef PLANCK_WATCHDOG_DISABLE
@@ -99,7 +84,7 @@ bool matrix_scan_custom(matrix_row_t current_matrix[]) {
         gpio_write_pin_high(matrix_col_pins[col]);
 
         // need wait to settle pin state
-        wait_us(PLANCK_ENCODER_SETTLE_PIN_DELAY);
+        wait_us(20);
 
         // read row data
         for (int row = 0; row < MATRIX_ROWS; row++) {
@@ -126,62 +111,21 @@ bool matrix_scan_custom(matrix_row_t current_matrix[]) {
     return changed;
 }
 
+#if defined(ENCODER_ENABLE) || defined(ENCODER_MAP_ENABLE)
+#if !defined(PLANCK_ENCODER_SETTLE_PIN_STATE_DELAY)
+#   define PLANCK_ENCODER_SETTLE_PIN_STATE_DELAY 10
+#endif
 
-#define ENCODER_CLOCKWISE true
-#define ENCODER_COUNTER_CLOCKWISE false
-
-static void encoder_handle_state_change(uint8_t index, uint8_t state) {
-    uint8_t i = index;
-
-    const uint8_t resolution = ENCODER_RESOLUTION;
-
-    encoder_pulses[i] += encoder_LUT[state & 0xF];
-
-    if (encoder_pulses[i] >= resolution) {
-        //printf("encoder_queue_event: %d, %d\n", index, ENCODER_COUNTER_CLOCKWISE);
-        encoder_queue_event(index, ENCODER_COUNTER_CLOCKWISE);
-    }
-
-    // direction is arbitrary here, but this clockwise
-    if (encoder_pulses[i] <= -resolution) {
-        //printf("encoder_queue_event: %d, %d\n", index, ENCODER_CLOCKWISE);
-        encoder_queue_event(index, ENCODER_CLOCKWISE);
-    }
-    encoder_pulses[i] %= resolution;
-
+void encoder_quadrature_init_pin(uint8_t index, bool pad_b) {
 }
 
-void encoder_driver_task(void) {
-
-    // set up C/rows for encoder read
-    for (int i = 0; i < MATRIX_ROWS; i++) {
-        gpio_set_pin_output(matrix_row_pins[i]);
-        gpio_write_pin_high(matrix_row_pins[i]);
-    }
-
-    // set up A & B for reading
-    gpio_set_pin_input_high(B12);
-    gpio_set_pin_input_high(B13);
-
-    for (int i = 0; i < MATRIX_ROWS; i++) {
-        gpio_write_pin_low(matrix_row_pins[i]);
-        wait_us(PLANCK_ENCODER_SETTLE_PIN_DELAY);
-        uint8_t new_status = (palReadPad(GPIOB, 12) << 0) | (palReadPad(GPIOB, 13) << 1);
-        if ((encoder_state[i] & 0x3) != new_status) {
-            encoder_state[i] <<= 2;
-            encoder_state[i] |= new_status;
-            encoder_handle_state_change(i, encoder_state[i]);
-        }
-        gpio_write_pin_high(matrix_row_pins[i]);
-    }
-
-    // revert A & B to matrix state
-    gpio_set_pin_input_low(B12);
-    gpio_set_pin_input_low(B13);
-
-    // revert C/rows to matrix state
-    for (int i = 0; i < MATRIX_ROWS; i++) {
-        gpio_set_pin_input_low(matrix_row_pins[i]);
-    }
-
+uint8_t encoder_quadrature_read_pin(uint8_t index, bool pad_b) {
+    pin_t col_pin = pad_b ? B13 : B12;
+    gpio_set_pin_output(col_pin);
+    gpio_write_pin_high(col_pin);
+    wait_us(PLANCK_ENCODER_SETTLE_PIN_STATE_DELAY);
+    uint8_t ret = gpio_read_pin(matrix_row_pins[index]) ? 0 : 1;
+    gpio_set_pin_input_low(col_pin);
+    return ret;
 }
+#endif // ENCODER_ENABLE || ENCODER_MAP_ENABLE
