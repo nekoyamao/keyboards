08d18a14b89400bdc241e3e1db8fe215603b48ed
nekoyamao <131568590+nekoyamao@users.noreply.github.com>
2024-09-22 18:20:56 +0900

    Update keymap.c

--- vertex/arc60s/keymaps/vial/keymap.c
+++ vertex/arc60s/keymaps/vial/keymap.c
@@ -53,9 +53,9 @@ void keyboard_post_init_user_td2(void) {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_00] = LAYOUT_all(
-        TD(0)  , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL , KC_GRV , KC_DEL ,
-        KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC,         KC_BSPC,
-        LGUI_T(KC_TAB),KC_A, KC_S, KC_D   , KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT, XXXXXXX, RGUI_T(KC_ENT),
+        TD(0)  , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_SCLN, KC_EQL , KC_GRV , KC_DEL ,
+        KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC,          KC_BSPC,
+        LGUI_T(KC_TAB),KC_A, KC_S, KC_D   , KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , KC_L   , KC_MINS, KC_QUOT, XXXXXXX, RGUI_T(KC_ENT),
         KC_LSFT, XXXXXXX, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH,          KC_RSFT, MO(4)  ,
         TD(1)  , KC_LCTL, LALT_T(KC_ENT), LSFT_T(KC_SPC),     LT(1,KC_ENT),              LT(2,KC_BSPC),    XXXXXXX, KC_RALT, KC_RGUI, TD(2)
     ),
@@ -69,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_02] = LAYOUT_all(
-        KC_NO  , KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_MINS, KC_EQL , KC_GRV , KC_DEL ,
+        KC_NO  , KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_SCLN, KC_EQL , KC_GRV , KC_DEL ,
         KC_TAB , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_LBRC, KC_RBRC,          KC_BSLS,
         KC_LGUI, KC_NO  ,LSFT(KC_EQL),KC_MINS,KC_EQL,KC_LPRN, KC_RPRN,KC_LEFT , KC_UP  , KC_DOWN, KC_RGHT, KC_NO  , XXXXXXX, KC_ENT ,
         KC_LSFT, XXXXXXX, KC_NO  ,LSFT(KC_8),KC_SLASH, KC_NO, KC_LBRC,KC_RBRC , KC_HOME, KC_PGUP, KC_PGDN, KC_END ,          KC_RSFT, KC_NO  ,
